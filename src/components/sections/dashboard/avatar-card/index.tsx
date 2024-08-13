21b687d09e8dba5ff03813591ef30b8f11a7105a
Riazul Islam <riazul5207@gmail.com>
2024-08-13 19:33:27 +0600

    avatar card component updated

--- src/components/sections/dashboard/avatar-card/index.tsx
+++ src/components/sections/dashboard/avatar-card/index.tsx
@@ -1,10 +1,36 @@
-import { Avatar, Paper, Stack, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 import AvatarImg from 'assets/images/avatars/Avatar.png';
 import IconifyIcon from 'components/base/IconifyIcon';
 
+const profile = {
+  name: 'Charles Robbie',
+  location: 'New York, USA',
+  avatar: AvatarImg,
+  details: [
+    {
+      id: 1,
+      type: 'Projects',
+      count: 28,
+    },
+    {
+      id: 2,
+      type: 'Followers',
+      count: 643,
+    },
+    {
+      id: 3,
+      type: 'Following',
+      count: 76,
+    },
+  ],
+};
+
 const AvatarCard = () => {
   return (
-    <Paper sx={{py: 4.75, height: 355}}>
+    <Paper sx={{ py: 4.75, height: 355 }}>
       <Stack direction="column" alignItems="center">
         <Avatar
           src={AvatarImg}
@@ -15,27 +41,25 @@ const AvatarCard = () => {
           }}
         />
 
-        <Typography variant='h4' color="text.primary" mt={2}>Charles Robbie</Typography>
+        <Typography variant="h4" color="text.primary" mt={2}>
+          {profile.name}
+        </Typography>
         <Stack mt={0.25} alignItems="center" spacing={0.5}>
-          <IconifyIcon icon="ic:outline-location-on" color="text.disabled" fontSize="h6.fontSize"/>
-          <Typography variant='body1' color="text.disabled">New York, USA</Typography>
+          <IconifyIcon icon="ic:outline-location-on" color="text.disabled" fontSize="h6.fontSize" />
+          <Typography variant="body1" color="text.disabled">
+            {profile.location}
+          </Typography>
         </Stack>
 
         <Stack mt={3} spacing={4}>
-          <Stack direction="column" alignItems="center">
-            <Typography color="text.disabled" fontSize="body2.fontSize">Projects</Typography>
-            <Typography variant='h4'>28</Typography>
-          </Stack>
-
-          <Stack direction="column" alignItems="center">
-            <Typography color="text.disabled" fontSize="body2.fontSize">Followers</Typography>
-            <Typography variant='h4'>643</Typography>
-          </Stack>
-
-          <Stack direction="column" alignItems="center">
-            <Typography color="text.disabled" fontSize="body2.fontSize">Following</Typography>
-            <Typography variant='h4'>76</Typography>
-          </Stack>
+          {profile.details.map((item) => (
+            <Stack key={item.id} direction="column" alignItems="center">
+              <Typography color="text.disabled" fontSize="body2.fontSize">
+                {item.type}
+              </Typography>
+              <Typography variant="h4">{item.count}</Typography>
+            </Stack>
+          ))}
         </Stack>
       </Stack>
     </Paper>
