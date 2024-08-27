37a6e59947a86d2e1160799161b2d8b0bb34f4fe
Riazul Islam <riazul5207@gmail.com>
2024-08-27 11:59:18 +0600

    auth layout updated

--- src/layouts/auth-layout/index.tsx
+++ src/layouts/auth-layout/index.tsx
@@ -1,6 +1,11 @@
 import { PropsWithChildren } from 'react';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import Image from 'components/base/Image';
+import Logo from 'assets/images/logo.png';
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
@@ -9,11 +14,23 @@ const AuthLayout = ({ children }: PropsWithChildren) => {
       alignItems="center"
       justifyContent="center"
       px={1}
-      py={7}
+      pt={12}
+      pb={10}
       width={1}
       minHeight="100vh"
     >
-      <Paper sx={{ py: 4, width: 1, maxWidth: 460 }}>{children}</Paper>
+      <ButtonBase
+        component={Link}
+        href="/"
+        disableRipple
+        sx={{ position: 'absolute', top: 24, left: 24 }}
+      >
+        <Image src={Logo} alt="logo" height={36} width={36} sx={{ mr: 1.5 }} />
+        <Typography variant="h4" color="text.primary" letterSpacing={1}>
+          VENUS
+        </Typography>
+      </ButtonBase>
+      <Paper sx={{ px: { xs: 2, sm: 3.5 }, py: 4, width: 1, maxWidth: 460 }}>{children}</Paper>
     </Stack>
   );
 };
